d9e5f2bcb6e0bfd41a2f59aa8f98abd6aa073431
williamcouto <willplacido40@gmail.com>
2024-06-20 10:03:46 -0300

    update JS

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function enviarForm(){
     let name_user = document.getElementById('name_user')
     let email = document.getElementById('email_user')
 
-    if(name_user.length > 30 && email.length == ''){
+    if(name_user.length > 20 && email.length == ''){
         window.alert("Enviado")
     }
 }
