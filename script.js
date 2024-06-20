4279b978e36e91392721cece7776421dc4536f11
williamcouto <willplacido40@gmail.com>
2024-06-20 19:59:11 -0300

    update HTML
    mudanças no HTML

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function enviarForm(){
     let name_user = document.getElementById('name_user')
     let email = document.getElementById('email_user')
+    let answ_1 = document.getElementById('asw1')
 
     if(name_user.length > 20 && email.length == ''){
         window.alert("Enviado")
