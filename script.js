e1e5795c5da388f22464b3db939a1756e1a65939
williamcouto <willplacido40@gmail.com>
2024-06-22 11:58:12 -0300

    update JS

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function enviarForm(){
     let name_user = document.getElementById('name_user')
     let email = document.getElementById('email_user')
     let sel_job = document.getElementById('status-job')
+    let user_comment = document.getElementById('user-comment')
 
-    if(name_user.length >= 10 && email.length != ''){
+    if(name_user.length >= 10 && email.length != '' && sel_job != ''){
         window.location = "./confirm-feed.html"
     }
 }
