2a5070a358ef3c070f602c63ed2318c3b3a2d89e
williamcouto <willplacido40@gmail.com>
2024-10-16 14:57:31 -0300

    fix buttons and fonts size

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function enviarForm(){
     let sel_job = document.getElementById('status-job')
     let user_comment = document.getElementById('user-comment')
 
-    if(name_user.length >= 10 && email.length != '' && sel_job != ''){
-        window.alert = "Mensagem Enviada com Sucesso!"
+    if(name_user.length >= 10 && email.length != "" && sel_job != ""){
+        window.alert = "Mensagem Enviada com Sucesso! Entraremos em contato!"
     }
 }
